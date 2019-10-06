ba0787eae39f71b19bb74ec99b18a0a2521b63ee
James Hunter Carter <james.carter@mythic.ai>
2019-10-05 20:44:46 -0700

    Added init function

--- customer.js
+++ customer.js
@@ -1,3 +1,35 @@
+function init(options) {
+    if (typeof options == 'object') {
+        if (options.Gist.hasOwnProperty('appId')) {
+            if (options.hasOwnProperty('gist')) {
+                /* Gist */
+                (function (d, h, w) {
+                    var gist = w.gist = w.gist || []
+                    gist.methods = ['trackPageView', 'identify', 'track', 'setAppId']
+                    gist.factory = function (t) {
+                        return function () {
+                            var e = Array.prototype.slice.call(arguments)
+                            e.unshift(t)
+                            gist.push(e)
+                            return gist
+                        }
+                    }
+                    for (var i = 0; i < gist.methods.length; i++) {
+                        var c = gist.methods[i]
+                        gist[c] = gist.factory(c)
+                    }
+                    s = d.createElement('script'), s.src = 'https://widget.getgist.com', s.async = !0, e = d.getElementsByTagName(h)[0], e.appendChild(s), s.addEventListener('load', function (e) {
+                    }, !1), gist.setAppId(options.gist.appId), gist.trackPageView()
+                })(document, 'head', window)
+            }
+        }
+    }
+    else {
+        return null;
+    }
+}
+
+
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
