66b95cfb583d89401acd67a3e4e7c06718acd899
Sahil Dawka <sahildawka@uvic.ca>
2019-10-19 22:33:49 -0400

    Added Countly support

--- customer.js
+++ customer.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 function initCrm(options) {
     if (typeof options == 'object') {
         if (options.hasOwnProperty('gist')) {
@@ -21,6 +23,49 @@ function initCrm(options) {
                     s = d.createElement('script'), s.src = 'https://widget.getgist.com', s.async = !0, e = d.getElementsByTagName(h)[0], e.appendChild(s), s.addEventListener('load', function (e) {}, !1), gist.setAppId(options.gist.appId), gist.trackPageView()
                 })(document, 'head', window)
                 console.log("Initialized Gist");
+            }
+        }
+        if (options.hasOwnProperty('countly')) {
+            if (options.countly.hasOwnProperty('appId') && options.countly.hasOwnProperty('url')) {
+                // Some default pre init
+                var Countly = Countly || {};
+                Countly.q = Countly.q || [];
+
+                // Provide your app key that you retrieved from Countly dashboard
+                Countly.app_key = options.countly.appId;
+
+                // Provide your server IP or name. Use try.count.ly or us-try.count.ly 
+                // or asia-try.count.ly for EE trial server.
+                // If you use your own server, make sure you have https enabled if you use
+                // https below.
+                Countly.url = options.countly.url;
+
+                // Start pushing function calls to queue
+                // Track sessions automatically (recommended)
+                Countly.q.push(['track_sessions']);
+
+                //track web page views automatically (recommended)
+                Countly.q.push(['track_pageview']);
+
+                // Uncomment the following line to track web heatmaps (Enterprise Edition)
+                // Countly.q.push(['track_clicks']);
+
+                // Uncomment the following line to track web scrollmaps (Enterprise Edition)
+                // Countly.q.push(['track_scrolls']);
+
+                // Load Countly script asynchronously
+                (function () {
+                    var cly = document.createElement('script');
+                    cly.type = 'text/javascript';
+                    cly.async = true;
+                    // Enter url of script here (see below for other option)
+                    cly.src = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@latest/lib/countly.min.js';
+                    cly.onload = function () {
+                        Countly.init()
+                    };
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(cly, s);
+                })();
             }
         }
     } else {
@@ -53,6 +98,9 @@ if (typeof ga == 'function') {
 }
 if (typeof heap == 'object') {
     detectedServices.push('heap');
+}
+if (typeof Countly !== 'undefined') {
+    detectedServices.push('countly');
 }
 
 /**
@@ -110,6 +158,22 @@ function identify(identifier, data) {
         }
     }
 
+    if (detectedServices.includes('countly')) {
+        Countly.device_id = identifier;
+        if (data) {
+            let countlyData = {};
+            const countlyKeys = ['name', 'username', 'email', 'organization', 'phone', 'picture', 'gender', 'byear'];
+            Object.keys(data).forEach(key => {
+                if (countlyKeys.includes(key)) {
+                    countlyData[key] = data[key];
+                } else {
+                    countlyData.custom[key] = data[key];
+                }
+            });
+            Countly.q.push(['user_details', countlyData]);
+        }
+    }
+
     var result = {};
     var input = {
         identifier: data
@@ -158,5 +222,14 @@ function sendEvent(eventName, eventObject) {
     // Heap
     if (detectedServices.includes('heap')) {
         heap.track(eventName, eventObject);
+    }
+
+    // Countly
+    if (detectedServices.includes('countly')) {
+        Countly.q.push(['add_event', {
+            "key": eventName,
+            "count": 1,
+            "segmentation": eventObject
+        }]);
     }
 }
